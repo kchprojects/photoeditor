2b7b47f4977050c7205f668770510182be5da1eb
Karel Chladek <email.karel.chladek@gmail.com>
2023-07-17 23:01:30 +0200

    invert filter

--- app/src/filter.py
+++ app/src/filter.py
@@ -13,7 +13,13 @@ class ToGrayscale(Filter):
             return img
         if len(img.shape) == 3 and img.shape[2] == 3:
             return cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    
+
+class Invert(Filter):
+    def __call__(self,img,selection=None):
+        if img is None:
+            return img
+        if len(img.shape) == 2 or (len(img.shape) == 3 and img.shape[2] == 3):
+            return 255-img
 
 
 all_filters = {f.__name__:f for f in Filter.__subclasses__()}
